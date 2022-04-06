3446c527d3eb58881e797a1dce12e3279c11b687
Walterly <45856199+leviOceans@users.noreply.github.com>
2022-04-06 12:45:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+function umbridgeGenerator(){
+    let counter = 0;
+    let umbridgePunishment = "";
+
+    while(counter < 100){
+        umbridgePunishment = umbridgePunishment + "I must not tell lies ";
+        counter++;
+    }
+    console.log(umbridgePunishment);
+    return umbridgePunishment;
+}
+    umbridgeGenerator();
+
